ac022c0aa444f99305a7a30614b6d447286cbf5f
yydick <yydick@sohu.com>
2020-04-19 13:05:33 +0800

    组织框架

---
+++ src/Main.php
@@ -0,0 +1,14 @@
+<?php
+declare(strict_types=1);
+/* 
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+namespace Spool\Pedis;
+
+class Main
+{
+    
+}
---
+++ tests/ExampleTest.php
@@ -0,0 +1,18 @@
+<?php
+
+declare(strict_types = 1);
+
+namespace Spool\Pedis\Tests;
+
+use PHPUnit\Framework\TestCase;
+
+class ExampleTest extends TestCase
+{
+
+    public function testEmpty()
+    {
+        echo "I'm in test!\n";
+        $this->assertTrue(TRUE);
+    }
+
+}
